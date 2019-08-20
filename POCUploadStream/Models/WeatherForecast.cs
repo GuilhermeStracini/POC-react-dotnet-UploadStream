6c722c3e4f9a67a0e31c9b6527c7fc869bf7fb0a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-08-20 16:57:38 -0300

    Fixed some architecture errors

---
+++ POCUploadStream/Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace POCUploadStream.Models
+{
+    public partial class SampleDataController
+    {
+        public class WeatherForecast
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
